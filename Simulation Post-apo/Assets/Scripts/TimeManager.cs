f31f4caa3c7052723f468981a3268af7b7b6515b
BRUCELIT-PC\Brucelit <simon.galand@gmail.com>
2016-11-14 17:33:25 +0100

    Mise en place de la vitesse
    Vitesse de la simulation variable avec un slider

---
+++ Simulation Post-apo/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour {
+
+    public Slider inputSlider;
+    public Text inputTime;
+    private float timeThreshold;
+    private float currentTime;
+    private System.DateTime currentDate;
+    
+    void Start ()
+    {
+        currentDate = System.DateTime.Now;
+        currentTime = Time.time;
+        timeThreshold = currentTime;
+        UpdateTime();
+	}
+	
+	void Update ()
+    {
+        Time.timeScale = inputSlider.value;
+
+        if(timeThreshold > currentTime + 10f)
+        {
+            currentDate = currentDate.AddDays(1);
+            UpdateTime();
+            currentTime = Time.time;
+            timeThreshold = currentTime;
+        }
+        else
+        {
+            timeThreshold += 0.05f * Time.timeScale;
+        }
+	}
+
+    void UpdateTime()
+    {
+        inputTime.text = "Time : " + currentDate.ToString();
+    }
+}
